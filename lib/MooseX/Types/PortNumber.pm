05e0dd49ff385a5a1428b73956624b8a45e98d26
Dave Rolsky <autarch@urth.org>
2016-08-31 10:17:21 -0500

    Bump version after release

--- lib/MooseX/Types/PortNumber.pm
+++ lib/MooseX/Types/PortNumber.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use MooseX::Types -declare =>
     [qw(PortNumber PortWellKnow PortRegistered PortPrivate)];
